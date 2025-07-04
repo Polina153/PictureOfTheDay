866f3723c8b2851c50288588b8d916e325460d24
Linka <Polina153@mail.ru>
2025-07-04 19:08:17 +0400

    5 методичка Animated Activity Transition 2 Explode

--- app/src/main/java/com/example/pictureoftheday/AnimationsActivity.kt
+++ app/src/main/java/com/example/pictureoftheday/AnimationsActivity.kt
@@ -1,14 +1,24 @@
 package com.example.pictureoftheday
 
+import android.annotation.SuppressLint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Fade
 import androidx.transition.Slide
+import androidx.transition.Transition
+import androidx.transition.TransitionListenerAdapter
 import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.example.pictureoftheday.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
@@ -29,12 +39,73 @@ class AnimationsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.recyclerView.adapter = Adapter()
+    }
 
-        binding.button.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,  Slide(Gravity.END))
-            textIsVisible = !textIsVisible
-            binding.text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+    /*private fun explode(clickedView: View) {
+        val viewRect = Rect()
+        clickedView.getGlobalVisibleRect(viewRect)
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition): Rect {
+                return viewRect
+            }
+        }
+        explode.duration = 3000
+        TransitionManager.beginDelayedTransition(binding.recyclerView, explode)
+        binding.recyclerView.adapter = null
+    }*/
+
+    private fun explode(clickedView: View) {
+        val viewRect = Rect()
+        clickedView.getGlobalVisibleRect(viewRect)
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition): Rect {
+                return viewRect
+            }
+        }
+        explode.duration = 3000
+        explode.excludeTarget(clickedView, true)
+        val set = TransitionSet()
+            .addTransition(explode)
+            .addTransition(Fade().addTarget(clickedView))
+            .addListener(object : TransitionListenerAdapter() {
+                override fun onTransitionEnd(transition: Transition) {
+                    transition.removeListener(this)
+                    onBackPressed()
+                }
+            })
+        TransitionManager.beginDelayedTransition(binding.recyclerView, set)
+        binding.recyclerView.adapter = null
+    }
+
+
+
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animations_explode_recycle_view_item,
+                    parent,
+                    false
+                ) as View
+            )
         }
 
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                explode(it)
+            }
+        }
+
+
+
+        override fun getItemCount(): Int {
+            return 32
+        }
     }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
