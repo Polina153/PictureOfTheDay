c17308ca7cdcb58f1fbe9f5b1e9d9314fd6c99c9
Linka <Polina153@mail.ru>
2025-08-02 16:50:43 +0400

    6 методичка Recycler view - изменение элемента списка

--- app/src/main/java/com/example/pictureoftheday/Data.kt
+++ app/src/main/java/com/example/pictureoftheday/Data.kt
@@ -1,6 +1,7 @@
 package com.example.pictureoftheday
 
 data class Data(
+    val id: Int = 0,
     val someText: String = "Text",
     val someDescription: String? = "Description"
 )
--- app/src/main/java/com/example/pictureoftheday/RecyclerActivity.kt
+++ app/src/main/java/com/example/pictureoftheday/RecyclerActivity.kt
@@ -36,27 +36,10 @@ class RecyclerActivity : AppCompatActivity() {
         }
 
         val data = arrayListOf(
-            /*Data("Earth"),
-            Data("Earth"),*/
-            Data("Mars", "")/*,
-            Data("Earth"),
-            Data("Earth"),
-            Data("Earth"),
-            Data("Mars", null),
-            Data("Earth"),
-            Data("Earth"),
-            Data("Earth"),
-            Data("Mars", null)*/
+            Pair(Data(1, "Mars", ""), false)//not sure!
         )
 
-        data.add(0, Data("Header"))
-
-        /* binding.recyclerView.addItemDecoration(
-             DividerItemDecoration(
-                 this,
-                 LinearLayoutManager.VERTICAL
-             )
-         )*/
+        data.add(0, Pair(Data(0, "Header"), false))//not sure!
 
         val adapter = RecyclerActivityAdapter(
             object : RecyclerActivityAdapter.OnListItemClickListener {
@@ -75,7 +58,7 @@ class RecyclerActivity : AppCompatActivity() {
 
     class RecyclerActivityAdapter(
         private var onListItemClickListener: OnListItemClickListener,
-        private var data: MutableList<Data>
+        private var data: MutableList<Pair<Data, Boolean>>
     ) :
         RecyclerView.Adapter<BaseViewHolder>() {
 
@@ -114,7 +97,7 @@ class RecyclerActivity : AppCompatActivity() {
             /*return if (data[position].someDescription.isNullOrBlank()) TYPE_MARS else TYPE_EARTH*/
             return when {
                 position == 0 -> TYPE_HEADER
-                data[position].someDescription.isNullOrBlank() -> TYPE_MARS
+                data[position].first.someDescription.isNullOrBlank() -> TYPE_MARS
                 else -> TYPE_EARTH
             }
         }
@@ -122,21 +105,20 @@ class RecyclerActivity : AppCompatActivity() {
         @SuppressLint("NotifyDataSetChanged")
         fun appendItem() {
             data.add(generateItem())
-            //notifyDataSetChanged()
             notifyItemInserted(itemCount - 1)
         }
 
-        private fun generateItem() = Data("Mars", "")
+        private fun generateItem() = Pair(Data(1, "Mars", ""), false)
 
         inner class EarthViewHolder(view: View) : BaseViewHolder(view) {
 
-            override fun bind(data: Data) {
+            override fun bind(dataItem: Pair<Data, Boolean>) {
                 if (layoutPosition != RecyclerView.NO_POSITION) {
                     itemView.findViewById<TextView>(R.id.descriptionTextView).text =
-                        data.someDescription
+                        dataItem.first.someDescription
                     itemView.findViewById<ImageView>(R.id.wikiImageView).setOnClickListener {
                         onListItemClickListener.onItemClick(
-                            data
+                            dataItem.first
                         )
                     }
                 }
@@ -152,9 +134,9 @@ class RecyclerActivity : AppCompatActivity() {
         }*/
 
 
-            override fun bind(data: Data) {
+            override fun bind(dataItem: Pair<Data, Boolean>) {
                 itemView.findViewById<ImageView>(R.id.marsImageView)
-                    .setOnClickListener { onListItemClickListener.onItemClick(data) }
+                    .setOnClickListener { onListItemClickListener.onItemClick(dataItem.first) }
                 itemView.findViewById<ImageView>(R.id.addItemImageView)
                     .setOnClickListener { addItem() }
                 itemView.findViewById<ImageView>(R.id.removeItemImageView)
@@ -163,7 +145,19 @@ class RecyclerActivity : AppCompatActivity() {
                     .setOnClickListener { moveDown() }
                 itemView.findViewById<ImageView>(R.id.moveItemUp)
                     .setOnClickListener { moveUp() }
+                itemView.findViewById<TextView>(R.id.marsDescriptionTextView).visibility =
+                    if (dataItem.second) View.VISIBLE else View.GONE
+                itemView.findViewById<TextView>(R.id.marsTextView).setOnClickListener { toggleText() }
+
             }
+
+            private fun toggleText() {
+                data[layoutPosition] = data[layoutPosition].let {
+                    it.first to !it.second
+                }
+                notifyItemChanged(layoutPosition)
+            }
+
 
             private fun moveUp() {
                 layoutPosition.takeIf { it > 1 }?.also { currentPosition ->
@@ -186,13 +180,11 @@ class RecyclerActivity : AppCompatActivity() {
 
             private fun addItem() {
                 data.add(layoutPosition, generateItem())
-                //notifyDataSetChanged()
                 notifyItemInserted(layoutPosition)
             }
 
             private fun removeItem() {
                 data.removeAt(layoutPosition)
-                //notifyDataSetChanged()
                 notifyItemRemoved(layoutPosition)
             }
         }
@@ -200,8 +192,8 @@ class RecyclerActivity : AppCompatActivity() {
 
         inner class HeaderViewHolder(view: View) : BaseViewHolder(view) {
 
-            override fun bind(data: Data) {
-                itemView.setOnClickListener { onListItemClickListener.onItemClick(data) }
+            override fun bind(dataItem: Pair<Data, Boolean>) {
+                itemView.setOnClickListener { onListItemClickListener.onItemClick(dataItem.first) }
             }
         }
 
